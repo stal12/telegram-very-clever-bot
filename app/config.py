1c3b14f5fee077da12ee50531285b07d6dfff2d2
Mandos39 <stefano.allegretti@hotmail.it>
2018-06-05 23:10:49 +0200

    modified config

--- app/config.py
+++ app/config.py
@@ -1,19 +1,7 @@
-token = '553797113:AAEZRVbAxkHvDs1xfeH4zNxYD2t3RJaC6Bs'
+token = 'token'
 
-url = 'https://marchettino-bot.appspot.com/HOOK'
+url = 'https://your-app.appspot.com/HOOK'
 
-list = ['Stramaledettamente anni 80',
-        'Dio va',
-        'Peerbacco',
-        'Che potenza, che classe, che stile',
-        'Pazzesco',
-        'Come sei pacchiano',
-        'Ma allooora',
-        'Viva la figa, chi la sperniga e con il baffo se l\'intriga',
-        'Ah beh ma... Sembra ma...',
-        'Ah beh... ma se mi dici che...',
-        'Dio va la Verrix',
-        'Dio va ma come rolla John Genga',
-        'Ah beh se le bomba tutte Pietro Maria la Nube Romano',
-        'Che storia'
+list = ['Hello',
+        'I\'m a good guy'
         ]
